bb4bf55921b21eb8b255f0141ea46bbcf0eebc0f
kjalba <albakevinjulian@gmail.com>
2025-05-26 18:46:51 -0500

    [fix]: added default opts to render-markdown.nvim to remove white highlight at bottom of triple backticks for code blocks

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -5,5 +5,33 @@ return {
   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
-  opts = {},
+  opts = {
+    code = {
+      enabled = true,
+      render_modes = false,
+      sign = true,
+      style = 'full',
+      position = 'left',
+      language_pad = 0,
+      language_icon = true,
+      language_name = true,
+      disable_background = { 'diff' },
+      width = 'full',
+      left_margin = 0,
+      left_pad = 0,
+      right_pad = 0,
+      min_width = 0,
+      border = 'hide',
+      above = '▄',
+      below = '▀',
+      inline_left = '',
+      inline_right = '',
+      inline_pad = 0,
+      highlight = 'RenderMarkdownCode',
+      highlight_language = nil,
+      highlight_border = 'RenderMarkdownCodeBorder',
+      highlight_fallback = 'RenderMarkdownCodeFallback',
+      highlight_inline = 'RenderMarkdownCodeInline',
+    },
+  },
 }
