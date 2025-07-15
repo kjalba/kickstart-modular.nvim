200a5053eeb4efba2f957ff2a52831fdfccc2ca1
kjalba <albakevinjulian@gmail.com>
2025-07-15 11:39:28 -0500

    updated conform and lspconfig to updated modular kickstart (new nvim version update changed some stuff) and got ruff and ruff server working for python

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -15,21 +15,20 @@ return {
     },
     opts = {
       notify_on_error = false,
+      -- Use the modern format_on_save logic
       format_on_save = function(bufnr)
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
         local disable_filetypes = { c = true, cpp = true }
-        local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
-          lsp_format_opt = 'never'
+          return nil
         else
-          lsp_format_opt = 'fallback'
+          return {
+            timeout_ms = 500,
+            lsp_format = 'fallback',
+          }
         end
-        return {
-          timeout_ms = 1000,
-          lsp_format = lsp_format_opt,
-        }
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
@@ -45,28 +44,15 @@ return {
         markdown = { 'prettier' },
         graphql = { 'prettier' },
         liquid = { 'prettier' },
-        python = { 'ruff' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        python = {
+          -- To fix auto-fixable lint errors.
+          'ruff_fix',
+          -- To run the Ruff formatter.
+          'ruff_format',
+          -- To organize the imports.
+          'ruff_organize_imports',
+        },
       },
-      -- formatters = {
-      --   prettier = {
-      --     prepend_args = function(self, ctx)
-      --       if ctx.filetype == 'markdown' then
-      --         return {
-      --           '--print-width',
-      --           '80',
-      --           '--prose-wrap',
-      --           'always',
-      --         }
-      --       end
-      --       return {}
-      --     end,
-      --   },
-      -- },
     },
   },
 }
