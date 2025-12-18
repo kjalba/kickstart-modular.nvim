13365cb1bdec9276bc6314bf550f81f3cacc2e8a
kjalba <albakevinjulian@gmail.com>
2025-12-18 16:24:02 -0600

    Remove vimtex plugin because I think it's causing RAM issues

--- lua/custom/plugins/vimtex.lua
+++ lua/custom/plugins/vimtex.lua
@@ -2,20 +2,20 @@
 ---
 return {
   {
-    'lervag/vimtex',
-    lazy = false,
-    -- tag = "v2.17", -- uncomment to pin to a specific release
-    init = function()
-      --global vimtex settings
-      vim.g.vimtex_imaps_enabled = 0 --i.e., disable them
-
-      --vimtex_view_settings
-      vim.g.vimtex_view_method = 'skim' -- <-- THIS IS THE ONLY LINE NEEDED
-
-      --quickfix settings
-      vim.g.vimtex_quickfix_open_on_warning = 0 --  don't open quickfix if there are only warnings
-      vim.g.vimtex_quickfix_ignore_filters =
-        { 'Underfull', 'Overfull', 'LaTeX Warning: .\\+ float specifier changed to', 'Package hyperref Warning: Token not allowed in a PDF string' }
-    end,
+    -- 'lervag/vimtex',
+    -- lazy = false,
+    -- -- tag = "v2.17", -- uncomment to pin to a specific release
+    -- init = function()
+    --   --global vimtex settings
+    --   vim.g.vimtex_imaps_enabled = 0 --i.e., disable them
+    --
+    --   --vimtex_view_settings
+    --   vim.g.vimtex_view_method = 'skim' -- <-- THIS IS THE ONLY LINE NEEDED
+    --
+    --   --quickfix settings
+    --   vim.g.vimtex_quickfix_open_on_warning = 0 --  don't open quickfix if there are only warnings
+    --   vim.g.vimtex_quickfix_ignore_filters =
+    --     { 'Underfull', 'Overfull', 'LaTeX Warning: .\\+ float specifier changed to', 'Package hyperref Warning: Token not allowed in a PDF string' }
+    -- end,
   },
 }
